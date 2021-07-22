44d20953f52b98c14152d3aafffb7a382f21fde1
almorgv <almorgv@gmail.com>
2021-07-22 17:50:03 +0300

    [rnd-tech-conf-21] current progress

---
+++ rnd-tech-conf-21/components/LineChartOnlyX.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+import { defineProps, computed } from "vue";
+const props = defineProps({
+  data: {
+    type: Object,
+  },
+  options: {
+    type: Object,
+  },
+});
+
+const chartOptions = computed(() => ({
+
+}));
+const options = {
+   scales: {
+      y: {
+         ticks: {
+            display: false,
+         }
+      }
+   }
+}
+</script>
+
+<template>
+  <line-chart
+  :data=data
+  :library=options
+  />
+</template>
---
+++ rnd-tech-conf-21/setup/main.ts
@@ -0,0 +1,31 @@
+import { defineAppSetup } from '@slidev/types'
+import VueChartkick from 'vue-chartkick'
+import 'chartkick/chart.js'
+import { Chart } from 'chart.js';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
+
+export default defineAppSetup(({ app, router }) => {
+    // Chart.register(ChartDataLabels);
+    VueChartkick.options = {
+        library: {
+            devicePixelRatio: 2,
+            // plugins: {
+            //     datalabels: {
+            //       backgroundColor: function(context) {
+            //         return context.dataset.backgroundColor;
+            //       },
+            //       borderRadius: 4,
+            //       color: 'white',
+            //       font: {
+            //         weight: 'bold'
+            //       },
+            //       formatter: function (values, context) {
+            //         return context.chart.data.labels[context.dataIndex];
+            //       },
+            //       padding: 6
+            //     }
+            //   },
+        }
+    }
+    app.use(VueChartkick)
+})
